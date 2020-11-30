7f5b894f5ca8089806f090cc27fe6a88c50dce66
Ramnik Singh <ramniksingh@MacBook-Pro-tilhrende-Ramnik.local>
2020-11-30 10:07:39 +0100

    Den tjekker nu om det er en email der logger in

--- Controller/userController.js
+++ Controller/userController.js
@@ -23,35 +23,52 @@ controller.post('/signup', (req, res, next) => {
     } else {
       bcrypt.hash(req.body.password, 10, (err, hash) => {
         if (err) {
-            return res.status(500).json({
-              error: err
-            }); 
+          return res.status(500).json({
+            error: err
+          }); 
         } else {
-            const user = new User({
-                _id: mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-              });
-              user
-              .save()
-              .then(result => {
-                  console.log(result);
-                  res.status(201).json({
-                    message: 'User created'
-                  });
-              })
-              .catch(err => {
-                  console.log(err);
-                  res.status(500).json({
-                    error: err 
-                  });
-
-              }); 
+          const user = new User({
+            _id: mongoose.Types.ObjectId(),
+            email: req.body.email,
+            password: hash
+          });
+          user
+          .save()
+          .then(result => {
+            console.log(result);
+            res.status(201).json({
+              message: 'User created'
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({
+              error: err 
+            });
+            
+          }); 
         }
-    }) 
+      }) 
     }
-
+    
   });
 });
 
-module.exports = controller; 
+controller.delete("/:userID", (req, res, next) => {
+  User.remove({_id: req.params.userID})
+  .exec()
+  .then(result => {
+    res.status(200).json({
+      message: "User deleted"
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err 
+      
+    });
+  });
+});
+
+module.exports = controller;
