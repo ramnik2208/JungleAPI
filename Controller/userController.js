03da34a6ea9fab82bd32e52929a2db2967f5240b
Ramnik Singh <ramniksingh@MacBook-Pro-tilhrende-Ramnik.local>
2020-12-03 20:04:15 +0100

    Jeg har nu lavet min html

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const controller = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt'); 
+const jwt = require ("jsonwebtoken");
 
 const User = require('../Model/user');
 
@@ -48,11 +49,57 @@ controller.post('/signup', (req, res, next) => {
             
           }); 
         }
-      }) 
+      });
     }
     
   });
 });
+
+
+controller.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            "secret",
+            {
+                expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed"
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
 
 controller.delete("/:userID", (req, res, next) => {
   User.remove({_id: req.params.userID})
