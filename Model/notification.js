f9c9690d0dd47d0a42a87d291f80b5eb366b0da6
ramnik2208 <ramnik_vairea@hotmail.com>
2020-12-09 12:12:10 +0100

    Added notification controller

--- Model/notification.js
+++ Model/notification.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose'); 
+
+const notificationSchema = mongoose.Schema({
+    _id = mongoose.Schema.Types.ObjectId,
+    message: String,
+    created_at: Date,
+    is_read: Boolean,
+    owner_id: String
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
+
+/*
+// when I get notification for a single user
+db.notifications.find({"owner": user_id, "read": {$ne: true}})
+*/
